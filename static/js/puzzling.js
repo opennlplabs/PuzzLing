6c697aa40e7b2c7e2caa2445d8162abda4435c50
Xiao Zhang <58770171+xiao11lam@users.noreply.github.com>
2022-07-06 05:02:23 -0700

    update the sharing and printing function

--- static/js/puzzling.js
+++ static/js/puzzling.js
@@ -3,10 +3,35 @@ function countChars(textarea) {
     var text = textarea.value;
     var len = text.length;
     var counter = document.getElementById('counter');
-    if (len < 2001) {
+    if (len < 100) {
         counter.textContent = len + '/100';
     } else {
         var locale = getCurrentLocale();
         counter.innerHTML = DICTIONARIES[locale]['Above maximum'] + ' <span style="color: red;">' + len + '</span>' + '/2000';
     }
 }
+
+
+ function display() {
+    window.print();
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
