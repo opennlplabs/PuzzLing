c844b4e3dac5c41fb56bc8f67bcef2b56beedc4a
Xiao Zhang <58770171+xiao11lam@users.noreply.github.com>
2022-07-07 02:19:01 -0700

    update the timer function

--- static/js/timer.js
+++ static/js/timer.js
@@ -1,25 +1,41 @@
-var timer = new easytimer.Timer();
-$('.buttons-wrapper .startButton').click(function () {
-    timer.start();
-});
+var time = 0;
+var running = 0;
 
-$('.buttons-wrapper .pauseButton').click(function () {
-    timer.pause();
-});
+function startPause() {
+    if (running == 0) {
+        running = 1;
+        increment();
+        document.getElementById("startPause").innerHTML = "PAUSE";
+    } else {
+        running = 0;
+        document.getElementById("startPause").innerHTML = "RESUME";
+    }
+}
 
-$('.buttons-wrapper .resetButton').click(function () {
-    timer.reset();
-    timer.stop();
-});
+function reset(){
+    running = 0;
+    time = 0;
+    document.getElementById("startPause").innerHTML = "START";
+    document.getElementById("output").innerHTML = "00:00:00";
+}
 
-timer.addEventListener('secondsUpdated', function (e) {
-    $('.time-values').html(timer.getTimeValues().toString());
-});
+function increment() {
+    if (running == 1) {
+        setTimeout(function() {
+            time++;
+            var mins = Math.floor(time/10/60);
+            var secs = Math.floor(time/10 % 60);
+            var tenths = time % 10;
+            if (mins < 10) {
+              mins = "0" + mins;
+            }
+            if (secs < 10) {
+              secs = "0" + secs;
+            }
+            document.getElementById("output").innerHTML = mins + ":" + secs + ":" + "0" + tenths;
+            increment();
+        },100);
+    }
+}
 
-timer.addEventListener('started', function (e) {
-    $('.time-values').html(timer.getTimeValues().toString());
-});
 
-timer.addEventListener('reset', function (e) {
-    $('.time-values').html(timer.getTimeValues().toString());
-});
