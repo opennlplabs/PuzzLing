cc94f42c50767052314a798ef4efbe1ec71b2740
Xiao Zhang <58770171+xiao11lam@users.noreply.github.com>
2022-07-12 00:17:39 -0700

    clean some codes there

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ import requests
 import json
 import argparse
 import random
+from calc_score import calc_score
+from get_translation import translate
 
-import torch
-from transformers import AutoModel, AutoTokenizer
 
 language_path = {
 					'Somali':		"./data/testing_set/demo.txt",
@@ -32,27 +32,18 @@ language_path = {
 				}
 
 
-tokenizer = AutoTokenizer.from_pretrained("princeton-nlp/sup-simcse-bert-base-uncased")
-model = AutoModel.from_pretrained("princeton-nlp/sup-simcse-bert-base-uncased")
-
-def sentence_mapping(sentence):
-	inputs = tokenizer(sentence, padding=True, truncation=True, return_tensors="pt")
-	with torch.no_grad():
-		embeddings = model(**inputs, output_hidden_states=True, return_dict=True).pooler_output
-
-	return embeddings
+url = 'https://platform.neuralspace.ai/api/translation/v1/annotated/translate'
+#auth_token =
+headers = {}
 
 flask_app = Flask(__name__)
 flask_app.secret_key = 'any random string'
 #run_with_ngrok(flask_app)
 
-def l2norm(x, dim=-1):
-	return x / x.norm(2, dim=dim, keepdim=True).clamp(min=1e-6)
 
 @flask_app.route("/", methods=['GET', 'POST'])
 def Home():
 	# read the testing corpus from the data/testing_set dir
-
 	language = request.args.get('language', None)
 	file_name = language_path[language]
 
@@ -97,51 +88,6 @@ def predict():
 		target = "{}".format(target),
 		language = "{}".format(language)
 		)
-
-
-def calc_score(inputs, target):
-	if isinstance(target, str):
-		target = [target]
-		inputs = [inputs]
-
-	assert len(target) == 1
-	assert len(inputs) == 1
-	inputs_repre = sentence_mapping(inputs)
-	target_repre = sentence_mapping(target)
-
-	score = similarity(l2norm(inputs_repre), l2norm(target_repre))
-
-	#score = random.random()
-
-	return score
-
-
-def similarity(inputs_repre, target_repre):
-	"""Cosine similarity between all the inputs and target pairs"""
-	return inputs_repre.mm(target_repre.t()).item()
-
-
-url = 'https://platform.neuralspace.ai/api/translation/v1/annotated/translate'
-#auth_token = 
-headers = {}
-
-
-def translate(sentence, languageToken="zh-CN"): 
-	passedValue = sentence.encode('utf-8').decode('latin1')
-	data = f"""
-	{{
-		"text": "{passedValue}",
-		"sourceLanguage":"{languageToken}",
-		"targetLanguage": "en"
-	}}
-	"""
-	resp = requests.post(url, headers=headers, data=data)
-
-	response_dict = json.loads(resp.text)
-	translatedtext = response_dict["data"]["translated_text"]
-
-	return translatedtext
-
 
 @flask_app.route("/files/")
 def render_the_files_page():
