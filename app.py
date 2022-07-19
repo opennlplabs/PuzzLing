951b664e4ff142a4c07bb0f8f7ffe071e855a2a4
Xiao Zhang <58770171+xiao11lam@users.noreply.github.com>
2022-07-19 09:48:02 -0700

    add the 404 page function and do some debuggings there

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ from pre_question import pre_question
 language_path = {
 					'Somali':		"./data/testing_set/demo.txt",
 					'Xhosa':		"./data/testing_set/demo.txt",
-					'Pashto':		"./data/testing_set/pashto/pashto.txt",
+					'Pashto':		"./data/testing_set/pashto/pashto_backup.txt",
 					'Twi':			"./data/testing_set/demo.txt",
 					'Ukrainian':	"./data/testing_set/demo.txt"
 				}
@@ -55,8 +55,8 @@ def Home():
 		#lines = random.sample(file.readlines(), 1)
 		# convert the list output into string
 		#lines = " ".join(lines)
-
-		question = lines
+		# return the question on the frontend
+		question = lines.split("$")[1]
 		# do the pre_processing of the question and return
 		# question = pre_question(lines)
 
@@ -66,8 +66,8 @@ def Home():
 		"index.html", 
 		question = "{}".format(question),
 		language = "{}".format(language),
-		index = "{}".format(str(index))
-		)
+		index = "{}".format(str(index)),
+	)
 
 
 @flask_app.route("/predict", methods=['GET', 'POST'])
@@ -77,12 +77,10 @@ def predict():
 	target = request.args.get('question')#request.args.get('question')
 	language = request.args.get('language', None)
 	index = request.args.get('index', -1)
+	index = int(index)
 
 	if index <= 0:
 		raise ValueError('Index should be larger than 0')
-		
-	index = int(index)
-
 
 	target = escape(target)#translate(escape(target),headers)
 	prediction_score = calc_score(sentence, target)
@@ -100,6 +98,11 @@ def predict():
 def render_the_files_page():
 	return render_template("files.html")
 
+
+@flask_app.errorhandler(404)
+def page_not_found(e):
+	# note that we set the 404 status explicitly
+    return render_template('404.html'), 404
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
