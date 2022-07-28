745c2e6b711ced064e20e0285b3a8c3dc42bf3a5
Xiao Zhang <58770171+xiao11lam@users.noreply.github.com>
2022-07-28 04:05:43 -0700

    add some quotes

--- apps/authentication/test_db.py
+++ apps/authentication/test_db.py
@@ -6,6 +6,12 @@ exectCmd = "SELECT rowid, * FROM Users;"
 
 # print all the rows from the database
 def readFronSqllite(db_path, exectCmd):
+    """
+
+    :param db_path:
+    :param exectCmd:
+    :return:
+    """
     conn = db.connect(db_path)
     cursor = conn.cursor()
     conn.row_factory = db.Row
